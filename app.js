c3bd9907416e04859f133e58ad6c467b4ef7b68e
mauroslucios <mauroslucios@gmail.com>
2021-09-18 20:31:10 -0300

    Adicionado style css mdbootstrap e rotas básicas update

---
+++ app.js
@@ -0,0 +1,30 @@
+//Carregando módulos
+    const express = require('express')
+    const handlebars = require('express-handlebars')
+    const bodyParser = require('body-parser')
+    const app = express();
+    const admin = require('./routes/admin')
+    const path = require("path")
+    //const mongoose = require('./database/connection')
+//Configurações
+    //Body Parser
+        app.use(bodyParser.urlencoded({extended: true}))
+        app.use(bodyParser.json())
+    //Handlebars
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
+    //Mongoose
+
+    //Public static
+        app.use(express.static(path.join(__dirname,"public")))
+
+//Rotas
+    app.get('/',(req,res)=>{
+        res.send("Rota principal")
+    })
+    app.use('/admin', admin)
+//Outros
+const PORT = 3001
+app.listen(PORT,()=>{
+    console.log(`Running on url: http://localhost:${PORT}`);
+})
---
+++ database/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://172.17.0.3:27017/blogapp').then(()=>{
+    console.log("Mongodb conectado...")
+}).catch((err)=>{
+    console.log("Houve um erro ao conectar: "+ err)
+})
+mongoose.Promise = global.Promise;
+module.exports = mongoose
