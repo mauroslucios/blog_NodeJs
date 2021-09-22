14111e2668d84ec8b9d24e47f9deadd29bde1c70
mauroslucios <mauroslucios@gmail.com>
2021-09-22 11:18:16 -0300

    List all categorias, views update

--- routes/admin.js
+++ routes/admin.js
@@ -13,21 +13,58 @@ router.get('/posts', (req,res)=>{
 })
 
 router.get('/categorias', (req, res)=>{
-    res.render("admin/categorias")
+    Categoria.find().sort({date:'desc'}).lean().then((categorias)=>{
+        res.render("admin/categorias", {categorias: categorias})
+    }).catch((err)=>{
+        req.flash("error_msg","Houve um erro ao listar as categorias")
+        res.redirect("/admin")
+        console.log("Erro ao listar categorias: "+ err)
+    })
+    
 })
 
 router.post("/categorias/nova", (req,res)=>{
-    const novaCategoria ={
-        nome: req.body.nome,
-        slug: req.body.slug
+
+    //Validação manual
+    let erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "Nome inválido!"})
     }
-    new Categoria(novaCategoria).save().then(()=>{
-        console.log("Categoria inserida com sucesso!")
-    }).catch((err)=>{
-        console.log("Houve um erro ao inserir a categoria!")
-    })
+
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push({texto: "Slug inválido"})
+    }
+
+    if(req.body.nome.length < 2){
+        erros.push({texto: "Tamanho deverá ser maior que 2 caracteres"})
+    }
+
+    if(req.body.slug.length < 2){
+        erros.push({texto: "Tamanho deverá ser maior que 2 caracteres"})
+    }
+
+    if(erros.length > 0){
+        res.render("admin/addcategorias",{erros: erros})
+    }else{
+
+        const novaCategoria ={
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+        new Categoria(novaCategoria).save().then(()=>{
+            req.flash("success_msg","Categoria criada com sucesso.")
+            res.redirect("/admin/categorias")
+        }).catch((err)=>{
+            req.flash("error_msg","Houve um erro ao inserir a categoria!")
+            res.redirect("/admin")
+        })  
+    }
 })
 
+router.get("/categorias/edit/:id",(req,res)=>{
+    res.send("Página de edição de categoria")
+})
 router.get('/categorias/add',(req,res)=>{
     res.render("admin/addcategorias")
 })
